30193c56703407495a0512ed6facdab2f52c1019
stzups <griffinht@gmail.com>
2021-09-27 20:08:53 -0400

    add verbose mode and start to add -n and -d

--- README.md
+++ README.md
@@ -10,10 +10,34 @@ dockron program
 ```
 dockron ./script.sh
 ```
-Run a command (program or script)
-
+Run a command (program or script).
+#
 ```
-echo "hello" | dockron program arg1 arg2 arg3
+echo "hello" | dockron program arg1 arg2 arg3 > file
 ```
-Run a command and pass along `stdin` and arguments
+Run the command `program` and pass along `stdin` and arguments, then write resulting `stdout` from `program` to file `file`.
+#
+```
+-n <amount>
+```
+Run program `amount` times.
 
+Defaults to `-n 1` to run program once.
+
+Set to a negative value `-n -1` to run program over and over.
+
+`-n 0` will do nothing, as the program will not be run.
+#
+```
+-d <milliseconds>
+```
+Wait `milliseconds` between each run, except for the first run which starts immediately.
+
+Defaults to `-d 0` to rerun immediately if `-n` is greater than `1`.
+#
+```
+-v
+```
+Enable verbose mode.
+
+Prints helpful debug information to `stderr`.
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,46 @@
 fn main() {
+    let mut n = 1; // amount of times to run
+    let mut delay = 0; // delay for runs except for the first
+    let mut verbose = false;
+
     let mut args: Vec<String> = std::env::args().collect();
     args.remove(0); // first arg is irrelevant?
-    let mut result = std::process::Command::new(args.remove(0))
-        .args(args)
+    let program = args.remove(0);
+    while args.get(0).unwrap().starts_with("-") {
+        let arg = args
+            .remove(0) // pop --arg
+            .chars()
+            .nth(1)// trim to arg
+            .unwrap()
+            .to_string();
+        let arg = arg.as_str();
+        match arg {
+            "n" => {
+                n = args.get(0).unwrap().parse().unwrap();
+            }
+            "d" => {
+                delay = args.get(0).unwrap().parse().unwrap();
+            }
+            "v" => {
+                verbose = true;
+            }
+            _=> {
+                eprintln!("unknown argument {}", arg);
+                std::process::exit(1);
+            }
+        }
+    }
+    if verbose { eprintln!("Running {} {} times with a {}ms delay with arguments {:?}", program, n, delay, args); }
+    let mut result = std::process::Command::new(program.as_str()) // program name
+        .args(args)// remaining args will be passed to child
         .spawn()
         .unwrap();
 
     let status = result
         .wait()
         .unwrap();
-    std::process::exit(status.code().unwrap());
+    let code = status.code().unwrap();
+    if verbose { eprintln!("{} exited with code {}", program, code) }
+    //todo repeat and delay
+    std::process::exit(code);
 }
