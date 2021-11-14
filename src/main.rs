aabda74c64f23cd983ba21d30ace37b1c5ad83ce
stzups <griffinht@gmail.com>
2021-11-14 11:25:45 -0500

    add version and help

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate getopts;
 
+const NAME: &str = "dockron";
+const VERSION: &str = "1.0";
+
 fn main() {
     let arguments: Vec<String>;
 
@@ -7,15 +10,30 @@ fn main() {
 
     let mut options = getopts::Options::new();
 
+    options.optflag("h", "help", "print help");
+    options.optflag("v", "version", "print version");
     options.optopt("n", "", "how many times to run", "<integer>");
     options.optopt("d", "delay", "delay of each run except for the first", "<milliseconds>");
-    options.optflag("v", "verbose", "verbose output, useful for debugging");
+    options.optflag("", "verbose", "verbose output, useful for debugging");
     options.optflag("i", "ignore", "ignore non-zero exit codes and keep running");
     let matches = match options.parse(&arguments[1..]) {
         Ok(m) => m,
         Err(error) => { eprintln!("{}", error); std::process::exit(1); }
     };
 
+    if matches.opt_present("h") {
+        eprint!("{}", options.usage_with_format(|opts| {
+            format!(
+                "Usage: dockron [options...] <command>\n{}\n",
+                opts.collect::<Vec<String>>().join("\n")
+            )
+        }));
+        return;
+    }
+    if matches.opt_present("v") {
+        eprintln!("{} version {}", NAME, VERSION);
+        return;
+    }
     let verbose = matches.opt_present("v");
     let n = match matches.opt_get_default("n", 1) {
         Ok(t) => t,
